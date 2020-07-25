e39fe54856e5b8c0738e4476619349de187427b3
Paco <frarosrod2@alum.us.es>
2020-07-25 12:19:33 +0200

    Minor fix

--- src/main/java/acme/entities/investmentRounds/InvestmentRound.java
+++ src/main/java/acme/entities/investmentRounds/InvestmentRound.java
@@ -1,11 +1,12 @@
 
 package acme.entities.investmentRounds;
 
+import java.util.Collection;
 import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -15,8 +16,8 @@ import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.URL;
 
+import acme.entities.activities.Activity;
 import acme.entities.roles.Entrepreneur;
-import acme.entities.workProgrammes.WorkProgramme;
 import acme.framework.datatypes.Money;
 import acme.framework.entities.DomainEntity;
 import lombok.Getter;
@@ -27,42 +28,42 @@ import lombok.Setter;
 @Setter
 public class InvestmentRound extends DomainEntity {
 
-	private static final long	serialVersionUID	= 1L;
+	private static final long		serialVersionUID	= 1L;
 
 	@NotBlank
-	private String				ticker;
+	private String					ticker;
 
 	@Temporal(TemporalType.TIMESTAMP)
 	@NotNull
-	private Date				creationMoment;
+	private Date					creationMoment;
 
 	@NotBlank
 	@Pattern(regexp = "^(SEED)?(ANGEL)?(SERIES-A)?(SERIES-B)?(SERIES-C)?(BRIDGE)?$")
-	private String				round;
+	private String					round;
 
 	@NotBlank
-	private String				title;
+	private String					title;
 
 	@NotBlank
-	private String				description;
+	private String					description;
 
 	@NotNull
 	@Valid
-	private Money				money;
+	private Money					money;
 
 	@URL
-	private String				url;
+	private String					url;
 
 	//Relations
 
 	@NotNull
 	@Valid
 	@ManyToOne(optional = false)
-	private Entrepreneur		entrepreneur;
+	private Entrepreneur			entrepreneur;
 
 	@NotNull
 	@Valid
-	@OneToOne(optional = false)
-	private WorkProgramme		workProgramme;
+	@OneToMany(mappedBy = "investmentRound")
+	private Collection<Activity>	activities;
 
 }
